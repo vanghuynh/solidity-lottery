94ccba0e395afababdd3b4c723181a1e507ad98d
vhuynh <vhuynh@gmail.com>
2023-12-09 16:42:27 +0700

    add unit test

--- deploy.js
+++ deploy.js
@@ -1,11 +1,11 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  "REPLACE_WITH_YOUR_MNEMONIC",
   // remember to change this to your own phrase!
-  'REPLACE WITH YOUR INFURA URL'
+  "REPLACE WITH YOUR INFURA URL"
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
@@ -13,11 +13,11 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: "1000000", from: accounts[0] });
 
   console.log(interface);
   console.log("Contract deployed to", result.options.address);
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,3 +1,61 @@
 const ganache = require("ganache");
 const { Web3 } = require("web3");
 // updated imports added for convenience
+const assert = require("assert");
+
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require("../compile");
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
+  });
+
+  it("allows one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.03", "ether"),
+    });
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+    assert.equal(players[0], accounts[0]);
+    assert.equal(players.length, 1);
+  });
+
+  it("allows multiple accounts to enter", async () => {
+    // first account
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.03", "ether"),
+    });
+    // second account
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.03", "ether"),
+    });
+    // third account
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei("0.03", "ether"),
+    });
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+    assert.equal(players[0], accounts[0]);
+    assert.equal(players[1], accounts[1]);
+    assert.equal(players[2], accounts[2]);
+    assert.equal(players.length, 3);
+  });
+});
